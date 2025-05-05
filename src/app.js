36fdde432a235fb53d04c1cc14db8f2742c48468
Shriyansh <shriyansh@Shriyanshs-MacBook-Air.local>
2025-05-06 01:15:57 +0530

    multiple route handlers and next()

--- src/app.js
+++ src/app.js
@@ -3,25 +3,30 @@ const express = require("express");
 const app = express();
 
 
-
-
-  
-app.get("/user/:id/profile", (req, res) => {
-    const {id} = req.params
-    // res.send(req.query)
-    res.send(`id is : ${id} and queried for ${JSON.stringify(req.query)}`)
-    // res.send(`id is : ${id} and queried for ${req.query}`)
-    // res.send({firstName: "Shriyansh", lastName: "Kumar"});
-})
-
-app.post("/user", (req, res) => {
-    // send data to the DB
-    res.send("The data is saved successfully!");
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully");
-})
+app.get("/user", 
+    (req, res, next) => {
+        // res.send("response!!")
+        console.log("rh1");
+        next()
+    }, 
+    (req, res, next) => {
+        console.log("rh2");
+        // res.send("response2!!")
+        next();
+    }
+)
+app.get("/user", 
+    [(req, res, next) => {
+        console.log("rh3");
+        // res.send("response3!!")
+        next()
+    }, 
+    (req, res, next) => {
+        console.log("rh4");
+        // next()
+        res.send("response4!!")
+    }]
+)
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000...");
