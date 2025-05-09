a5aacb0181980a6ed34a36d507a2698a29cee671
Shriyansh <shriyansh@Shriyanshs-MacBook-Air.local>
2025-05-10 02:36:33 +0530

    removed the uri from db.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose")
 
-const uri = "mongodb+srv://shriyansh:shriyansh12345@namastenode.crklcdw.mongodb.net/devTinder";
-
 const connectDB = async () => {
     await mongoose.connect(uri);
 };
-
 
 module.exports = {
     connectDB
